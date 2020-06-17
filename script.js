a9ecfc9392a38a649a3bbb37355e0236ddf781bd
fakeboyas <ovoivi9@gmail.com>
2020-06-17 22:10:16 +0700

    fixing script edit and delete

--- script.js
+++ script.js
@@ -1,84 +1,75 @@
-const list = [];
+let list = [];
 
-const addButton = document.getElementById("action1");
+//membuat function push
 
-// function addToDo() {
-//     const inputToDo = document.getElementById("add").value;
-//     list.push(inputToDo);
-// }
-
-addButton.addEventListener("click", function show() {
-    let displayOff = document.getElementById("display");
-    displayOff.innerHTML = null;
-    const inputToDo = document.getElementById("add").value;
-    list.push(inputToDo);
-    list.forEach(function display(item, index) {
-        let display1 = document.getElementById("display");
-        let text = document.createTextNode(item);
-
-        // merge text
-
-        let addText = document.createElement("li");
-        addText.appendChild(text);
-        addText.classList.add("show2");
-        let addButton1 = document.createElement("button");
-        let textButton1 = document.createTextNode("Edit");
-        let addButton2 = document.createElement("button");
-        let textButton2 = document.createTextNode("Delete");
-
-        addButton1.addEventListener("click", function () {
-            editTodo(index);
-        });
-
-        //  merge button1
-        addButton1.appendChild(textButton1);
-        //merger button2
-        addButton2.appendChild(textButton2);
-        //add div untuk styling
-        let btnDiv = document.createElement("div");
-        btnDiv.appendChild(addButton1);
-        btnDiv.appendChild(addButton2);
-        btnDiv.classList.add("btn");
-        //merge all
-
-        addText.appendChild(btnDiv);
-        display1.appendChild(addText);
-    });
-});
-
-//alternatif
-// addButton.addEventListener("click", function () {
-//     const inputToDo = document.getElementById("add").value;
-//     list.push(inputToDo);
-//     for (i = 0; i < list.length; i++) {
-//         var display1 = document.getElementById("display");
-//         var text = document.createTextNode(list[i]);
-//         var addText = document.createElement("li");
-//         var addButton1 = document.createElement("button");
-//         var textButton1 = document.createTextNode("Edit");
-//         var addButton2 = document.createElement("button");
-//         var textButton2 = document.createTextNode("Delete");
-//         var btndiv = document.createElement("div");
-//         // merge text
-//     }
-
-//     addText.appendChild(text);
-//     addText.classList.add("show2");
-//     //merge button1
-//     addButton1.appendChild(textButton1);
-//     //merger button2
-//     addButton2.appendChild(textButton2);
-//     //merge all
-//     display1.appendChild(addText);
-//     btndiv.appendChild(addButton1);
-//     btndiv.appendChild(addButton2);
-//     addText.appendChild(btndiv);
-//     display1.classList.add("show1");
-// });
-
-// membuat edit
+function addlist() {
+    let inputTodo = document.getElementById("add").value;
+    list.push(inputTodo);
+}
+//membuat function edit
 function editTodo(index) {
+    clear();
     let newText = prompt("Masukan Todo Yang Baru");
     list.splice(index, 1, newText);
-    show;
+    list.forEach(display);
 }
+
+// membuat function delete
+function deleteList(index) {
+    clear();
+    list.splice(index, 1);
+    list.forEach(display);
+}
+
+//membuat fungsi clear biar ngga double
+function clear() {
+    let displayOff = document.getElementById("display");
+    displayOff.innerHTML = null;
+}
+
+// membuat fungsi add todo
+function display(item, index) {
+    let display1 = document.getElementById("display");
+    let text = document.createTextNode(item);
+    let addText = document.createElement("li");
+    addText.appendChild(text);
+    addText.classList.add("show2");
+    let addButton1 = document.createElement("button");
+    let textButton1 = document.createTextNode("Edit");
+    let addButton2 = document.createElement("button");
+    let textButton2 = document.createTextNode("Delete");
+
+    addButton1.addEventListener("click", function () {
+        editTodo(index);
+    });
+
+    addButton2.addEventListener("click", function () {
+        deleteList(index);
+    });
+
+    //  merge button1
+    addButton1.appendChild(textButton1);
+    //merger button2
+    addButton2.appendChild(textButton2);
+    //add div untuk styling
+    let btnDiv = document.createElement("div");
+    btnDiv.appendChild(addButton1);
+    btnDiv.appendChild(addButton2);
+    btnDiv.classList.add("btn");
+    //merge all
+
+    addText.appendChild(btnDiv);
+    display1.appendChild(addText);
+}
+
+//membuat fungsi menampilkan
+function show() {
+    clear();
+    addlist();
+    list.forEach(display);
+}
+//add click
+
+let addButton = document.getElementById("action1");
+
+addButton.addEventListener("click", show);
