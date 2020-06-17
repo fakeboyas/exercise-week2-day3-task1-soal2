477bb8c3b4cefbec5ee31747aea37f92732bdd9a
fakeboyas <ovoivi9@gmail.com>
2020-06-17 14:29:12 +0700

    fix css

--- script.js
+++ script.js
@@ -7,18 +7,43 @@ function addToDo() {
     list.push(inputToDo);
 }
 
+addButton.addEventListener("click", function () {
+    const inputToDo = document.getElementById("add").value;
+    list.push(inputToDo);
+    list.forEach(function display(a, b) {
+        let display1 = document.getElementById("display");
+        let item = document.createTextNode(a);
+
+        // merge text
+
+        let addText = document.createElement("li");
+        addText.appendChild(item);
+        addText.classList.add("show2");
+
+        let addButton1 = document.createElement("button");
+        let textButton1 = document.createTextNode("Edit");
+        let addButton2 = document.createElement("button");
+        let textButton2 = document.createTextNode("Delete");
+        //merge button1
+        addButton1.appendChild(textButton1);
+        //merger button2
+        addButton2.appendChild(textButton2);
+
+        //merge all
+
+        addText.appendChild(addButton1);
+        addText.appendChild(addButton2);
+        display1.appendChild(addText);
+    });
+});
+
 // addButton.addEventListener("click", function () {
 //     const inputToDo = document.getElementById("add").value;
 //     list.push(inputToDo);
-//     list.forEach(function display(a, b) {
+//     for (i = 0; i < list.length; i++) {
 //         let display1 = document.getElementById("display");
-//         let item = document.createTextNode(a);
-
-//         // merge text
-
+//         let item = document.createTextNode(list[i]);
 //         let addText = document.createElement("span");
-//         addText.appendChild(item);
-
 //         let addButton1 = document.createElement("button");
 //         let textButton1 = document.createTextNode("Edit");
 //         let addButton2 = document.createElement("button");
@@ -34,30 +59,5 @@ function addToDo() {
 //         display1.appendChild(addText);
 //         display1.appendChild(addButton1);
 //         display1.appendChild(addButton2);
-//     });
+//     }
 // });
-
-addButton.addEventListener("click", function () {
-    const inputToDo = document.getElementById("add").value;
-    list.push(inputToDo);
-    for (i = 0; i < list.length; i++) {
-        let display1 = document.getElementById("display");
-        let item = document.createTextNode(list[i]);
-        let addText = document.createElement("span");
-        let addButton1 = document.createElement("button");
-        let textButton1 = document.createTextNode("Edit");
-        let addButton2 = document.createElement("button");
-        let textButton2 = document.createTextNode("Delete");
-
-        // merge text
-        addText.appendChild(item);
-        //merge button1
-        addButton1.appendChild(textButton1);
-        //merger button2
-        addButton2.appendChild(textButton2);
-        //merge all
-        display1.appendChild(addText);
-        display1.appendChild(addButton1);
-        display1.appendChild(addButton2);
-    }
-});
