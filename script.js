6437c6223cae74ba854a6183f5a75a86d75f0277
fakeboyas <ovoivi9@gmail.com>
2020-06-17 13:54:47 +0700

    first Commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const list = [];
+
+const addButton = document.getElementById("action1");
+
+function addToDo() {
+    const inputToDo = document.getElementById("add").value;
+    list.push(inputToDo);
+}
+
+// addButton.addEventListener("click", function () {
+//     const inputToDo = document.getElementById("add").value;
+//     list.push(inputToDo);
+//     list.forEach(function display(a, b) {
+//         let display1 = document.getElementById("display");
+//         let item = document.createTextNode(a);
+
+//         // merge text
+
+//         let addText = document.createElement("span");
+//         addText.appendChild(item);
+
+//         let addButton1 = document.createElement("button");
+//         let textButton1 = document.createTextNode("Edit");
+//         let addButton2 = document.createElement("button");
+//         let textButton2 = document.createTextNode("Delete");
+
+//         // merge text
+//         addText.appendChild(item);
+//         //merge button1
+//         addButton1.appendChild(textButton1);
+//         //merger button2
+//         addButton2.appendChild(textButton2);
+//         //merge all
+//         display1.appendChild(addText);
+//         display1.appendChild(addButton1);
+//         display1.appendChild(addButton2);
+//     });
+// });
+
+addButton.addEventListener("click", function () {
+    const inputToDo = document.getElementById("add").value;
+    list.push(inputToDo);
+    for (i = 0; i < list.length; i++) {
+        let display1 = document.getElementById("display");
+        let item = document.createTextNode(list[i]);
+        let addText = document.createElement("span");
+        let addButton1 = document.createElement("button");
+        let textButton1 = document.createTextNode("Edit");
+        let addButton2 = document.createElement("button");
+        let textButton2 = document.createTextNode("Delete");
+
+        // merge text
+        addText.appendChild(item);
+        //merge button1
+        addButton1.appendChild(textButton1);
+        //merger button2
+        addButton2.appendChild(textButton2);
+        //merge all
+        display1.appendChild(addText);
+        display1.appendChild(addButton1);
+        display1.appendChild(addButton2);
+    }
+});
